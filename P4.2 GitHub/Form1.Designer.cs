ec82adcebf7901977c27ee28c00b4653c95fcef3
joseluisgavilan@outlook.com <joseluis.gavilan@iesmarenostrum.com>
2017-02-13 17:46:40 +0100

    color boton

--- P4.2 GitHub/Form1.Designer.cs
+++ P4.2 GitHub/Form1.Designer.cs
@@ -33,12 +33,13 @@
             // 
             // button1
             // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
             this.button1.Location = new System.Drawing.Point(64, 65);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
             this.button1.Text = "Hola";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
